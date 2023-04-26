786416147c7f78db5cf18ae4efa6739204f0922f
ygorp <ygor-nogueira@hotmail.com>
2023-04-25 23:31:10 -0300

    Criando layout do sistema para o bairro serra dourada 2 referente ao projeto da faculdade PEI 2

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Comunica SD2</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+    <nav class="container">
+        <h1>Comunica Serra Dourada</h1>
+        <ul class="menu">
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Sobre</a></li>
+            <li><a href="">Noticias</a></li>
+            <li><a href="">Projetos</a></li>
+            <li><a href="">Divulgação</a></li>
+        </ul>
+        <div class="contatos">
+            <a href=""><i class="fa-brands fa-whatsapp"></i></a>
+            <a href=""><i class="fa-brands fa-instagram"></i></a>
+        </div>
+    </nav>
+</header>
+
+<main class="content">
+    <section class="sobre">
+        <h2>Sobre</h2>
+        <p>O bairro Serra Dourada II está inserido nos 3.266.085 metros quadrados que também compõem os conjuntos habitacionais Serra Dourada I e III e começou a ser habitado em 1982.
+            Habitantes: 6. 532. Limites: Parque Residencial Tubarão / Novo Porto Canoa / Serra Dourada I e III. Serra Dourada II está inserida na área de 3 milhões 266 mil e 85 metros
+            quadrados que compõem os Conjuntos Habitacionais Serra Dourada I, II e III, aprovados em 1979. Existiam 1.299 unidades cadastradas no bairro. Elas estão divididas em 1.050
+            residências, 95 terrenos vazios e 145 estabelecimentos comerciais.</p>
+        <img src="img/img_map_sd2.png" alt="Imagem do mapa de serra dourada 2">
+    </section>
+</main>
+</body>
+</html>
